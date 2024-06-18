1cee948bc7f4ba8fe17cbefcf29d7e9b759977c6
Neto <netovitu@gmail.com>
2024-06-18 10:46:27 -0300

    pug get auto changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\tools\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Fatec\Documents\AndroidStudioProjects\facul_map"
+export "FLUTTER_ROOT=C:\Users\joaon\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Flutter\faculmap"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
