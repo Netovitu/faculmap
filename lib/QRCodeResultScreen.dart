1aa8e81752a8acf6fda42bf22fb4c971810de699
Neto <netovitu@gmail.com>
2024-06-18 09:25:46 -0300

    removendo geolocator, não deu certo

--- lib/QRCodeResultScreen.dart
+++ lib/QRCodeResultScreen.dart
@@ -1,90 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
-class QRCodeResultScreen extends StatefulWidget {
+class QRCodeResultScreen extends StatelessWidget {
   final String codigo;
 
-  const QRCodeResultScreen({super.key, required this.codigo});
-
-  @override
-  _QRCodeResultScreenState createState() => _QRCodeResultScreenState();
-}
-
-class _QRCodeResultScreenState extends State<QRCodeResultScreen> {
-  Position? _currentPosition;
-  late final String _imagePath;
-
-  final double latMin = -23.564;
-  final double latMax = -23.560;
-  final double lonMin = -46.652;
-  final double lonMax = -46.650;
-  final double imgWidth = 1000; // Substitua pelo valor real da largura da imagem
-  final double imgHeight = 1000; // Substitua pelo valor real da altura da imagem
-
-  @override
-  void initState() {
-    super.initState();
-    _imagePath = _getImagePath(widget.codigo);
-    _getCurrentLocation();
-  }
-
-  Future<void> _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Verifique se os serviços de localização estão ativados
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Serviços de localização não estão ativados, pode mostrar um alerta
-      return;
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissões negadas, pode mostrar um alerta
-        return;
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissões negadas permanentemente, pode mostrar um alerta
-      return;
-    }
-
-    // Obtenha a localização atual do usuário
-    Position position = await Geolocator.getCurrentPosition();
-    setState(() {
-      _currentPosition = position;
-    });
-
-    // Monitore a posição do usuário e atualize conforme ele se move
-    Geolocator.getPositionStream().listen((Position position) {
-      setState(() {
-        _currentPosition = position;
-      });
-    });
-  }
+  const QRCodeResultScreen({Key? key, required this.codigo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final String imagePath = _getImagePath(codigo);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Mapa da faculdade", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.red,
       ),
-      body: _imagePath.isNotEmpty
-          ? Stack(
-        children: [
-          Center(child: Image.asset(_imagePath, fit: BoxFit.contain)),
-          if (_currentPosition != null)
-            Positioned(
-              left: lonToPixel(_currentPosition!.longitude, imgWidth, lonMin, lonMax),
-              top: latToPixel(_currentPosition!.latitude, imgHeight, latMin, latMax),
-              child: const Icon(Icons.person_pin_circle, color: Colors.red, size: 40.0),
-            ),
-        ],
+      body: imagePath.isNotEmpty
+          ? Center(
+        child: Image.asset(imagePath, fit: BoxFit.contain),
       )
           : const Center(
         child: Text(
@@ -96,21 +28,15 @@ class _QRCodeResultScreenState extends State<QRCodeResultScreen> {
   }
 
   String _getImagePath(String codigo) {
+    // Substitua este método pelo mapeamento real de códigos para imagens
     switch (codigo) {
       case "0001":
         return 'assets/maps/mapa_1.png';
       case "0002":
         return 'assets/maps/mapa_2.png';
+    // Adicione mais casos conforme necessário
       default:
         return '';
     }
-  }
-
-  double latToPixel(double latitude, double imgHeight, double latMin, double latMax) {
-    return imgHeight * (latitude - latMin) / (latMax - latMin);
-  }
-
-  double lonToPixel(double longitude, double imgWidth, double lonMin, double lonMax) {
-    return imgWidth * (longitude - lonMin) / (lonMax - lonMin);
   }
 }
